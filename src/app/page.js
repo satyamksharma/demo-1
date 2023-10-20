3f625f421ab0c07130a4d094a80ae4f9d9260a12
Satyam Kumar <ksatyam433@gmail.com>
2023-10-20 16:14:15 +0530

    update: sidebar static elements

--- src/app/page.js
+++ src/app/page.js
@@ -40,22 +40,34 @@ export default function Home() {
 
             <section className=''>
                 {/* sidebar */}
-                <aside className=' fixed flex flex-col justify-between bg-primary-50 h-full w-48 my-12 py-6 px-2 z-20'>
+                <aside className=' fixed flex flex-col justify-between bg-primary-50 h-full w-48 my-16 py-6 px-2 z-20'>
                     <div className=' text-sm'>
                         <button className='flex'>
                             <img
                                 src='./down.svg'
                                 alt='down'
                             />
-                            <span className='active:font-semibold'>Settings</span>
+                            <span className='active:font-semibold font-semibold'>Settings</span>
                         </button>
-                        <ul className=' ml-5'>
-                            <li className='active:font-semibold'>Members</li>
-                            <li className='active:font-semibold'>Company Details</li>
-                            <li className='active:font-semibold'>Career Page</li>
+                        <ul className=' ml-5 '>
+                            <li className='active:font-semibold font-semibold cursor-pointer'>
+                                Members
+                            </li>
+                            <li className='active:font-semibold cursor-pointer'>Company Details</li>
+                            <li className='active:font-semibold cursor-pointer'>Career Page</li>
                         </ul>
                     </div>
-                    <div className=''>ff</div>
+                    <ul className=' mx-2 text-sm flex flex-col gap-y-8'>
+                        <li className='flex px-2'>
+                            <img
+                                src='./mail.svg'
+                                alt='mail'
+                                className=' my-auto w-3 h-3'
+                            />
+                            <span className=' text-gray-400 mx-1'>help@plumes.ai</span>
+                        </li>
+                        <li className=' text-gray-400 font-semibold'> &copy; Plumes Copyright</li>
+                    </ul>
                     <div className=''></div>
                 </aside>
                 {/* main content */}
